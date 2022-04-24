bd45a10691c9795ced06ae21a0e66ea0ff65f448
Sergey Salomatin <salomatinsv8@gmail.com>
2022-04-24 10:03:31 +0300

    disabled trailing_whitespace created PlaceModel

--- PlaceFinder/MainViewController.swift
+++ PlaceFinder/MainViewController.swift
@@ -8,9 +8,8 @@
 import UIKit
 
 class MainViewController: UITableViewController {
-    let restaurantNames = ["McDonalds",
-                           "KFC",
-                           "Burger King"]
+
+    let places = Place.getPlaces()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,28 +17,30 @@ class MainViewController: UITableViewController {
     }
     // MARK: - Table view data source    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? CustomTableViewCell else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-            cell.textLabel?.text = restaurantNames[indexPath.row]
-            cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+            cell.textLabel?.text = places[indexPath.row].name
+            cell.imageView?.image = UIImage(named: places[indexPath.row].name)
             cell.imageView?.layer.cornerRadius = cell.frame.size.height/2
             cell.imageView?.clipsToBounds = true
             return cell
         }
-        cell.nameLabel?.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel?.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace?.image = UIImage(named: places[indexPath.row].name)
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height/2
         cell.imageOfPlace?.clipsToBounds = true
         return cell
     }
     // MARK: - Table view delegate
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 100
+//    }
 
    /*
     // MARK: - Navigation
